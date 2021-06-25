d836b610f4fd7567f4f1897eb3ad8a23ca8eb46e
BlueCoder5 <77952614+BlueCoder5@users.noreply.github.com>
2021-06-25 18:43:29 +1000

    Added basic boss battles

--- AdventureGame.java
+++ AdventureGame.java
@@ -19,8 +19,8 @@ public class AdventureGame {
         int enemyAttackDamage = 50;
 
         // Player
-        int playerHealth = 150;
-        int maxPlayerHealth = 150;
+        int playerHealth = 1000;
+        int maxPlayerHealth = 1000;
         int playerAttackDamage = 40;
         int playerRunChanceWithoutHit = 90; // Percentage
         String playerName = "";
@@ -29,6 +29,15 @@ public class AdventureGame {
         int numOfHealthPotions = 3;
         int healthPotionHealAmount = 25;
         int healthPotionDropChance = 20; // Percentage
+
+        //boss
+        String[] bosses = {"Three-Headed Dragon", "Giant Alien"};
+        int maxBossHealth = 300;
+        int minBossHealth = 80; 
+        int bossAttackDamage = 80;
+        int bossEncounter = 3;
+        int bossOffset = 3;
+        int bossScore = 0; 
 
         // Game
         boolean running = true;
@@ -42,7 +51,7 @@ public class AdventureGame {
         while(true) {
             String confirmation = in.next();
             if(confirmation.toLowerCase().equals("no")) {
-                endGame(in, playerName, score, turn);
+                endGame(in, playerName, score, turn, bossScore);
             } else if(confirmation.toLowerCase().equals("yes")) {
                 System.out.println("\nGoodluck " + playerName + " you'll need it!!!");
                 System.out.println(divider + "\n");
@@ -54,6 +63,69 @@ public class AdventureGame {
 
         GAME:
         while(running) {
+
+            if(score == bossEncounter) {
+                int bossHealth = rand.nextInt((maxBossHealth - minBossHealth) + 1) + minBossHealth;
+                String bossName = bosses[rand.nextInt(bosses.length)];
+
+                System.out.println(divider);
+                System.out.println("\tBoss: " + bossName + " has appeared!!!\n");
+
+                while(bossHealth > 0) {
+                    turn++;
+
+                    System.out.println("\tYour HP: " + playerHealth);
+                    System.out.println("\t" + bossName + "'s HP: " + bossHealth);
+                    System.out.println("\n\tWhat would you like to do?");
+                    System.out.println("\t1. Attack");
+                    System.out.println("\t2. Drink health potion");
+
+                    String input = in.next();
+                    if(input.equals("1")) {
+                        int damageDealt = rand.nextInt(playerAttackDamage);
+                        int damageTaken = rand.nextInt(bossAttackDamage);
+
+                        bossHealth -= playerAttackDamage;
+                        playerHealth -= bossAttackDamage;
+
+                        System.out.println("\tYou strike the " + bossName + " for " + damageDealt + " damage.");
+                        System.out.println("\tYou receive " + damageTaken + " damage from the " + bossName + "\n");
+
+                        if(bossHealth <= 0) {
+                            System.out.println("\tThe " + bossName + " was defeated!!!\n");
+                            System.out.println(divider);
+                            bossScore++;
+                            bossEncounter += bossOffset;
+                        } else if(playerHealth <= 0) {
+                            System.out.println("\tYou have died!\n");
+                            endGame(in, playerName, score, turn, bossScore);
+                        }
+                    } else if(input.equals("2")) {
+                        if(numOfHealthPotions > 0) {
+                            if(playerHealth < maxPlayerHealth) {
+                                if(playerHealth + healthPotionHealAmount >= maxPlayerHealth) {
+                                    playerHealth = maxPlayerHealth;
+                                } else {
+                                    playerHealth += healthPotionHealAmount;
+                                }
+                                numOfHealthPotions--;
+                                System.out.println("\tYou drink a health potion, you have healed for " + healthPotionHealAmount + ".");
+                                System.out.println("\tYou now have " + playerHealth + " HP.");
+                                System.out.println("\tYou have " + numOfHealthPotions + " health potions left.\n"); 
+                            } else {
+                                System.out.println("\tYou are at max health!\n");
+                            }
+                        } else {
+                            System.out.println("\tYou have no health potions left! Defeat enemies for a chance to get one.\n");
+                        }
+                    } else {
+                        System.out.println("\tInvalid Command!\n");
+                    }
+
+                }
+
+            }
+
             int enemyHealth = rand.nextInt((maxEnemyHealth - minEnemyHealth) + 1) + minEnemyHealth;
             String enemy = enemies[rand.nextInt(enemies.length)];
 
@@ -79,12 +151,11 @@ public class AdventureGame {
 
                     System.out.println("\tYou strike the " + enemy + " for " + damageDealt + " damage.");
                     System.out.println("\tYou receive  " + damageTaken + " damage from the " + enemy + "\n");
-                    if(playerHealth <= 0) {
-                        break;
-                    }
                     if(enemyHealth <= 0) {
                         System.out.println("\t" + enemy + " was defeated!");
                         score++;
+                    } else if(playerHealth <= 0) {
+                        break;
                     }
                 } else if(input.equals("2")) {
                     if(numOfHealthPotions > 0) {
@@ -125,7 +196,7 @@ public class AdventureGame {
             }
             if(playerHealth <= 0) {
                 System.out.println("\tYou have died!\n");
-                endGame(in, playerName, score, turn);
+                endGame(in, playerName, score, turn, bossScore);
             }
             
             System.out.println("---------------------------------------------");
@@ -153,18 +224,20 @@ public class AdventureGame {
                 System.out.println("\tYou continue on your adventure!\n");
             } else if(userInput.equals("2")) {
                 System.out.println("\tYou exit the cave, successful from your adventures!\n");
-                endGame(in, playerName, score, turn);
+                endGame(in, playerName, score, turn, bossScore);
             }
 
         }
-            endGame(in, playerName, score, turn);
+            endGame(in, playerName, score, turn, bossScore);
 
     }
 
-    public static void endGame(Scanner in, String playerName, int score, int turn) {
+    // end of game credits and stats
+    public static void endGame(Scanner in, String playerName, int score, int turn, int bossScore) {
         System.out.println("###############");
         System.out.println("\tTHANKS FOR PLAYING! " + playerName);
         System.out.println("\tYou earned a score of: " + score);
+        System.out.println("\tYou defeated " + bossScore + " bosses.");
         System.out.println("\tTurn: " + turn);
         System.out.println("###############");
         in.close();
